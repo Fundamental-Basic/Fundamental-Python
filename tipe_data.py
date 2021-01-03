75956610930c0b0ef66b4b6c2645c940a1d52d71
dikiamust <75531983+dikiamust@users.noreply.github.com>
2021-01-03 17:56:09 +0700

    Update .gitignore

---
+++ tipe_data.py
@@ -0,0 +1,34 @@
+# TIPE DATA STRING / DATA TYPE OF STRING
+
+a = 'hello World', 'Wake Up !'
+print( a )
+
+# TIPE DATA NUMBERS / DATA TYPE OF NUMBERS
+b, c = 102, 1.2
+
+print( b, f'=102', c, f'= 1.2' )
+
+# TIPE DATA LIST / DATA TYPE OF LIST
+d = [2, 3, 'you']
+e = d[2]
+f = [11, 12, 13, 14 ,15, 16]
+ff = f [2:3]
+
+print( f"2, 3, you =", d )
+print( f'you =', e )
+print(f'13 =', ff)
+
+# TIPE DATA TUPLE / DATA TYPE OF TUPLE
+g = (1, 2, 'hello')
+i = (100, 200, 300)
+print( g )
+print( i )
+
+# TIPE DATA DICTIONARY / DATA TYPE OF DICTIONARY
+j = {'key': 'value'}
+print( f'value =', j['key'] )
+#k =  # next example
+
+# TIPE DATA SET / DATA TYPE OF SET
+l = {1, 2, 3, 4, 5}
+print(l)
